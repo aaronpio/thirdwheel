4cc522f1137391f31ba5c4876929e0f5918cab5a
aaron <aaronpiolang@gmail.com>
2020-02-14 19:21:29 -0500

    Started database seeds file

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -5,3 +5,31 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require ('faker')
+cities = ['Montreal', 'Toronto', 'Quebec City']
+
+
+Gender.create(name: "Male", description: "")
+Gender.create(name: "Female",description: "")
+Gender.create(name: "Non-binary", description: "")
+genders = Gender.all
+
+40.times do |i|
+  User.create(
+    email: Faker::Internet.email,
+    name: Faker::Name.name,
+    password: 'password',
+    age: rand(18..40),
+    points: rand(0..1000),
+    bio: Faker::Hipster.paragraph(sentence_count: 4),
+    city: cities.sample,
+    gender: Gender.find(rand(1..3)),
+    image_url: '',
+    is_looking: true
+  )
+
+  LookingFor.create(
+    user: User.find(i+1),
+    gender: genders.sample(rand(1..2))
+  )
+end
