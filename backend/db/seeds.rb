7ba6ae71b22ef0fbc43736c9d56ef83b41f147c4
Vincent-Carrier <vincentcarrier15@gmail.com>
2020-02-14 19:45:04 -0500

    Fix db seeds

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -14,22 +14,65 @@ Gender.create(name: "Female",description: "")
 Gender.create(name: "Non-binary", description: "")
 genders = Gender.all
 
+pictures = [
+  "https://randomuser.me/api/portraits/women/34.jpg",
+  "https://randomuser.me/api/portraits/men/40.jpg",
+  "https://randomuser.me/api/portraits/men/3.jpg",
+  "https://randomuser.me/api/portraits/men/46.jpg",
+  "https://randomuser.me/api/portraits/men/77.jpg",
+  "https://randomuser.me/api/portraits/women/79.jpg",
+  "https://randomuser.me/api/portraits/women/7.jpg",
+  "https://randomuser.me/api/portraits/men/57.jpg",
+  "https://randomuser.me/api/portraits/women/8.jpg",
+  "https://randomuser.me/api/portraits/men/39.jpg",
+  "https://randomuser.me/api/portraits/men/21.jpg",
+  "https://randomuser.me/api/portraits/men/36.jpg",
+  "https://randomuser.me/api/portraits/women/83.jpg",
+  "https://randomuser.me/api/portraits/women/61.jpg",
+  "https://randomuser.me/api/portraits/men/42.jpg",
+  "https://randomuser.me/api/portraits/men/67.jpg",
+  "https://randomuser.me/api/portraits/men/34.jpg",
+  "https://randomuser.me/api/portraits/women/33.jpg",
+  "https://randomuser.me/api/portraits/men/18.jpg",
+  "https://randomuser.me/api/portraits/men/52.jpg",
+  "https://randomuser.me/api/portraits/men/93.jpg",
+  "https://randomuser.me/api/portraits/men/26.jpg",
+  "https://randomuser.me/api/portraits/women/47.jpg",
+  "https://randomuser.me/api/portraits/men/13.jpg",
+  "https://randomuser.me/api/portraits/men/84.jpg",
+  "https://randomuser.me/api/portraits/women/81.jpg",
+  "https://randomuser.me/api/portraits/men/33.jpg",
+  "https://randomuser.me/api/portraits/women/0.jpg",
+  "https://randomuser.me/api/portraits/men/11.jpg",
+  "https://randomuser.me/api/portraits/men/86.jpg",
+  "https://randomuser.me/api/portraits/men/19.jpg",
+  "https://randomuser.me/api/portraits/women/33.jpg",
+  "https://randomuser.me/api/portraits/men/18.jpg",
+  "https://randomuser.me/api/portraits/men/6.jpg",
+  "https://randomuser.me/api/portraits/men/91.jpg",
+  "https://randomuser.me/api/portraits/men/93.jpg",
+  "https://randomuser.me/api/portraits/men/40.jpg",
+  "https://randomuser.me/api/portraits/men/49.jpg",
+  "https://randomuser.me/api/portraits/women/80.jpg",
+  "https://randomuser.me/api/portraits/women/88.jpg",
+]
+
 40.times do |i|
   User.create(
     email: Faker::Internet.email,
-    name: Faker::Name.name,
+    name: Faker::Name.first_name,
     password: 'password',
     age: rand(18..40),
     points: rand(0..1000),
     bio: Faker::Hipster.paragraph(sentence_count: 4),
     city: cities.sample,
     gender: Gender.find(rand(1..3)),
-    image_url: '',
+    image_url: pictures[i],
     is_looking: true
   )
 
   LookingFor.create(
     user: User.find(i+1),
-    gender: genders.sample(rand(1..2))
+    gender: genders.sample
   )
 end
