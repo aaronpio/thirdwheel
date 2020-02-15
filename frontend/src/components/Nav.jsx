2044be5764e4873b85019d5187db98434ba7bd86
Vincent-Carrier <vincentcarrier15@gmail.com>
2020-02-15 14:02:08 -0500

    More basic CSS

--- frontend/src/components/Nav.jsx
+++ frontend/src/components/Nav.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "./Nav.scss";
 
 export default function Nav({ user }) {
   return (
     <nav>
-      <h1 className="logo">ThirdWheel</h1>
+      <Link to="/"><h1 className="logo">ThirdWheel</h1></Link>
       {user && <Link to="/profile">My Profile</Link>}
     </nav>
   );
--- frontend/src/components/Profile.jsx
+++ frontend/src/components/Profile.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "./Profile.scss"
 
 export default function Profile({ user }) {
   return (
