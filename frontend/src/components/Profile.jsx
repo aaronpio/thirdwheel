e0cc36ebe5e9310df1f92edebb9e6d2846d27891
Vincent-Carrier <vincentcarrier15@gmail.com>
2020-02-17 12:30:27 -0500

    CSS fixes

--- frontend/src/components/Profile.jsx
+++ frontend/src/components/Profile.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import "./Profile.module.scss"
+import "./Profile.module.scss";
 
 export default function Profile({ user }) {
   return (
-    <>
+    <article>
       <h3>Age</h3>
       <p>{user.age}</p>
 
@@ -14,9 +14,8 @@ export default function Profile({ user }) {
       <h3>Identifies as</h3>
       <p>{user.gender}</p>
 
-      <h3>Looking for</h3>
-      <p>{user.looking_for}</p>
-    </>
+      <h3>Bio</h3>
+      <p>{user.bio}</p>
+    </article>
   );
 }
-
