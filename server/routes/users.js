886d0b7e731cc0e976342ce464d99206a637aa13
Vincent-Carrier <vincentcarrier15@gmail.com>
2020-02-18 18:00:55 -0500

    Add user routes

--- server/routes/users.js
+++ server/routes/users.js
@@ -2,17 +2,24 @@ const express = require("express");
 const router = express.Router();
 
 const userRouter = db => {
-  /* GET users listing. */
-  router.get("/", function(req, res, next) {
-    res.send("respond with a resource");
-  });
-
   router.get("/shuffle", function(req, res) {
-    const users = db
-      .query("SELECT * FROM users LIMIT 6")
+    db.query("SELECT * FROM users ORDER BY RANDOM() LIMIT 6")
       .then(_res => res.json(_res.rows))
       .catch(err => console.log(err));
   });
+
+  router.get("/random", function(req, res) {
+    db.query("SELECT * FROM users ORDER BY RANDOM() LIMIT 1")
+      .then(_res => res.json(_res.rows[0]))
+      .catch(err => console.log(err));
+  });
+
+  router.get("/:id", function(req, res) {
+    db.query(`SELECT * FROM users WHERE id = ${req.params.id}`)
+      .then(_res => res.json(_res.rows[0]))
+      .catch(err => console.log(err));
+  })
+
 
   return router;
 };
