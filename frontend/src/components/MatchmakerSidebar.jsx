f43b5a1156965aa7b936e4214ba27422b4410140
Vincent-Carrier <vincentcarrier15@gmail.com>
2020-02-17 19:45:17 -0500

    Merged Picture and PicturePlaceholder

--- frontend/src/components/MatchmakerSidebar.jsx
+++ frontend/src/components/MatchmakerSidebar.jsx
@@ -1,13 +1,12 @@
 import React from "react";
 import "./MatchmakerSidebar.module.scss";
 import Picture from "./Picture";
-import PicturePlaceholder from "./PicturePlaceholder";
 
 export default function MatchmakerSidebar({ picks }) {
   return (
     <>
-      {picks[0] ? <Picture user={picks[0]} /> : <PicturePlaceholder />}
-      {picks[1] ? <Picture user={picks[1]} /> : <PicturePlaceholder />}
+      <Picture user={picks[0]} />
+      <Picture user={picks[1]} />
     </>
   );
 }
--- frontend/src/components/Picture.jsx
+++ frontend/src/components/Picture.jsx
@@ -2,22 +2,27 @@ import React from "react";
 import styles from "./Picture.module.scss";
 
 export default function Picture({ user }) {
-  const { name, image_url } = user;
   const style = {
-    backgroundImage: `url(${image_url})`,
+    backgroundColor: "#beeee2",
+    backgroundImage: `url(${user?.image_url})`,
     backgroundSize: "contain",
     borderRadius: "228px",
     boxShadow: "inset 8px 8px 0px rgba(0, 0, 0, 0.25)",
     width: "228px",
     height: "228px",
     margin: "auto",
-    display: "flex"
+    display: "block",
+    textAlign: "center",
+    fontFamily: "'Lobster Two', cursive",
+    margin: "auto",
+    fontSize: "180px",
+    color: "#cffff4"
   };
 
   return (
     <div className={styles.container}>
-      <div style={style}></div>
-      <h2 className={styles.picname}>{name}</h2>
+      <div style={style}>{!user && "?"}</div>
+      <h2 className={styles.picname}>{user ? user.name : "Take your pick"}</h2>
     </div>
   );
 }
