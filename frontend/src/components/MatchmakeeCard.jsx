186b08856e48ed003a462d515da03e5f38f2308c
Vincent-Carrier <vincentcarrier15@gmail.com>
2020-02-20 18:02:40 -0500

    Tweak matchmakee card CSS

--- frontend/src/components/MatchmakeeCard.jsx
+++ frontend/src/components/MatchmakeeCard.jsx
@@ -2,23 +2,13 @@ import React from "react";
 import styles from "./MatchmakeeCard.module.scss";
 
 const MatchmakeeCard = ({ user }) => {
-  const picStyle = {
-    width: "14rem",
-    height: "14rem",
-    borderRadius: "200px",
-    boxShadow: "2px 4px 8px rgba(0, 0, 0, 0.15)",
-    position: "relative",
-    bottom: "15px",
-    right: "-60px"
-  };
-
   return (
     <li className={styles.card}>
       <div>
         <h2 className="name">{user.name}, {user.age}</h2>
         <p align="left"> {user.bio}</p>
       </div>
-      <img style={picStyle} src={user?.image_url}></img>
+      <img src={user?.image_url}></img>
     </li>
   );
 };
