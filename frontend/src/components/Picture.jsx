69fae8391658cd594e24e4af997801aa28d13e1d
Vincent-Carrier <vincentcarrier15@gmail.com>
2020-02-21 12:11:19 -0500

    More CSS changes

--- frontend/src/components/Picture.jsx
+++ frontend/src/components/Picture.jsx
@@ -7,7 +7,9 @@ export default function Picture({ user }) {
     backgroundImage: `url(${user?.image_url})`,
     backgroundSize: "contain",
     borderRadius: "228px",
-    boxShadow: "2px 4px 8px rgba(0, 0, 0, 0.4)",
+    boxShadow: `2px 4px ${user ? "8px" : "2px"} rgba(0, 0, 0, 0.${
+      user ? "4" : "1"
+    })`,
     width: "228px",
     height: "228px",
     margin: "auto",
@@ -15,8 +17,7 @@ export default function Picture({ user }) {
     textAlign: "center",
     fontFamily: "'Lobster Two', cursive",
     fontSize: "180px",
-    color: "white", //<= question mark color
-    border: `${user ? "solid" : "dashed"} 8px white`
+    color: "white" //<= question mark color
   };
 
   return (
