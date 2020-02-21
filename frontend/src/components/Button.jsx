72700f3e85cf6fea5d1ab4e1b582ab4dca220686
aaron <aaronpiolang@gmail.com>
2020-02-21 11:49:18 -0500

    Fix profile sidebar links/button; started seperating matchmaker/profile sidebar css

--- frontend/src/components/Button.jsx
+++ frontend/src/components/Button.jsx
@@ -1,9 +1,19 @@
 import React from "react";
 import styles from "./Button.module.scss";
+//import className from "classnames";
 
-export default function Button({ shuffle, match, onClick, children, disabled }) {
+export default function Button({
+  shuffle,
+  match,
+  onClick,
+  children,
+  disabled
+}) {
   return (
-    <button onClick={!disabled && onClick} className={styles.button + ' ' + (disabled && styles.disabled)}>
+    <button
+      onClick={!disabled && onClick}
+      className={styles.button + " " + (disabled && styles.disabled)}
+    >
       {children}
     </button>
   );
