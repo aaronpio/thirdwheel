806577ca877090faceb746675bf6cdaf68914b15
aaron <aaronpiolang@gmail.com>
2020-02-21 14:42:24 -0500

    Added matchmaking filter SQL (left/right joins), and changed getmatches & getdates query to show newest matches first

--- server/routes/users.js
+++ server/routes/users.js
@@ -5,14 +5,18 @@ const { msleep } = require("sleep");
 const userRouter = db => {
   router.get("/shuffle", function(req, res) {
     msleep(500);
-    db.query("SELECT * FROM users ORDER BY RANDOM() LIMIT 6")
+    db.query(
+      "SELECT u2.* FROM sexual_preferences sp LEFT JOIN users u1 ON u1.id = sp.user_id RIGHT JOIN users u2 ON u2.gender_id = sp.gender_id WHERE u1.gender_id IN (SELECT sexual_preferences.gender_id FROM sexual_preferences JOIN users on u2.id = user_id) AND u1 != u2 AND u1.id = 1 ORDER BY RANDOM() LIMIT 6"
+    )
       .then(cursor => res.json(cursor.rows))
       .catch(err => console.log(err));
   });
 
   router.get("/random", function(req, res) {
     msleep(500);
-    db.query("SELECT * FROM users ORDER BY RANDOM() LIMIT 1")
+    db.query(
+      "SELECT u2.* FROM sexual_preferences sp LEFT JOIN users u1 ON u1.id = sp.user_id RIGHT JOIN users u2 ON u2.gender_id = sp.gender_id WHERE u1.gender_id IN (SELECT sexual_preferences.gender_id FROM sexual_preferences JOIN users on u2.id = user_id) AND u1 != u2 AND u1.id = 1 ORDER BY RANDOM() LIMIT 1"
+    )
       .then(cursor => res.json(cursor.rows[0]))
       .catch(err => console.log(err));
   });
@@ -23,7 +27,7 @@ const userRouter = db => {
       .then(cursor => {
         const user = cursor.rows[0];
         user.sexualPreferences = prefsCursor.rows;
-        console.log(user)
+        console.log(user);
         res.json(user);
       })
       .catch(err => console.log(err));
