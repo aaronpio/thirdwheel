1dada984a1526faf6664fe3572452c3371bb528b
aaron <aaronpiolang@gmail.com>
2020-02-22 12:40:23 -0500

    Add bounce-out animation when match selection confirmed

--- frontend/src/components/MatchmakerSidebar.jsx
+++ frontend/src/components/MatchmakerSidebar.jsx
@@ -1,19 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
+import classNames from "classnames";
 import styles from "./MatchmakerSidebar.module.scss";
 import Picture from "./Picture";
 import FloatingButton from "./FloatingButton";
 import Button from "./Button";
+import { useEffect } from "react";
 
 export default function MatchmakerSidebar({
   picks,
   removeFromSidebar,
   confirmMatch
 }) {
+  const [animate, setAnimate] = useState(false);
+
+  const confirmMatchAndAnimate = () => {
+    setAnimate(styles.bounceOut);
+    setTimeout(() => {
+      setAnimate(false);
+      confirmMatch();
+    }, 1000);
+  };
+
   return (
     <article className={styles.aside}>
       {picks.map((pick, index) => {
         return (
-          <div key={index}>
+          <div key={index} className={animate}>
             <Picture user={pick} text={"Take your pick"} />
             {pick && (
               <FloatingButton
@@ -25,7 +37,10 @@ export default function MatchmakerSidebar({
           </div>
         );
       })}
-      <Button disabled={!(picks[0] && picks[1])} onClick={confirmMatch}>
+      <Button
+        disabled={!(picks[0] && picks[1])}
+        onClick={confirmMatchAndAnimate}
+      >
         It's a Match
       </Button>
     </article>
