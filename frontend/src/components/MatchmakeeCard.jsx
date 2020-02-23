803f0b4db5ce3c7da2c39118f59096af307262d6
aaron <aaronpiolang@gmail.com>
2020-02-23 13:40:44 -0500

    Add gender symbols to cards

--- frontend/src/components/MatchmakeeCard.jsx
+++ frontend/src/components/MatchmakeeCard.jsx
@@ -2,12 +2,28 @@ import React from "react";
 import styles from "./MatchmakeeCard.module.scss";
 
 const MatchmakeeCard = ({ user, onClick }) => {
+  let gender_image;
+  switch (user.gender_id) {
+    case 1:
+      gender_image = "M-symbol.png";
+      break;
+    case 2:
+      gender_image = "F-symbol.png";
+      break;
+    case 3:
+      gender_image = "NB-symbol.png";
+      break;
+  }
+
   return (
     <li className={styles.card} onClick={onClick}>
       <div>
-        <h2 className="name">
-          {user.name}, {user.age}
-        </h2>
+        <header>
+          <h2 className="name">
+            {user.name}, {user.age}
+            <img className={styles.img_symbol} src={gender_image} />
+          </h2>
+        </header>
         <p align="left"> {user.bio}</p>
       </div>
       <img src={user?.image_url}></img>
